6a126888aca0b151b728031f643288ee6d6c328d
choatwattana <63130045@kmitl.ac.th>
2021-08-20 17:50:49 +0700

    Add exercise from lession2(week3)

---
+++ exercise/2008-2.2(2).py
@@ -0,0 +1,4 @@
+width = float(input())
+height = float(input())
+area = width * height
+print("The area of the rectangle is", area, "square units.")
---
+++ exercise/2008-2.4(4).py
@@ -0,0 +1,3 @@
+radius = float(input())
+area = 3.14 * radius ** 2
+print("The area of the circle", area, "square units.")
